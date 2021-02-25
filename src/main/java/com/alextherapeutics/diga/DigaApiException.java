2f7f353ed2af531ae3196fbf60f5106927d7655b
fongie <mkorlinge@hotmail.com>
2021-02-25 14:24:35 +0100

    improve diga api exception

--- src/main/java/com/alextherapeutics/diga/DigaApiException.java
+++ src/main/java/com/alextherapeutics/diga/DigaApiException.java
@@ -7,10 +7,10 @@ public class DigaApiException extends Exception {
     public DigaApiException(String msg) {
         super(msg);
     }
-    public DigaApiException(Exception e) {
-        super("Exception thrown: " + e.toString());
+    public DigaApiException(Throwable e) {
+        super("Exception thrown: " + e.toString(), e);
     }
-    public DigaApiException(Exception e, String msg) {
-        super("Exception thrown: " + e.toString() + "\nWith message: " + msg);
+    public DigaApiException(Throwable e, String msg) {
+        super("Exception thrown: " + e.toString() + "\nWith message: " + msg, e);
     }
 }
