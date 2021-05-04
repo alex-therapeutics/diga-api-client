8b02340ec7072633269a825f85972e179ef13912
fongie <mkorlinge@hotmail.com>
2021-05-04 14:56:07 +0200

    add ability to cast errors to specific error classes closes #37

---
+++ src/test/java/com/alextherapeutics/diga/model/DigaApiResponseErrorTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2021-2021 Alex Therapeutics AB and individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ *
+ */
+
+package com.alextherapeutics.diga.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigaApiResponseErrorTest {
+
+    @Test
+    void asCodeValidationError() {
+        DigaApiResponseError error = new DigaCodeValidationResponseError(DigaCodeValidationErrorCode.CODE_NOT_FOUND, "Code not found");
+        DigaApiResponseError nullError = new DigaApiExceptionError(new Exception());
+        assertNull(nullError.asCodeValidationError());
+        assertTrue(error.asCodeValidationError() instanceof DigaCodeValidationResponseError);
+    }
+
+    @Test
+    void asInvoiceResponseError() {
+        DigaApiResponseError error = DigaInvoiceResponseError.builder()
+                .messages("error")
+                .build();
+        DigaApiResponseError nullError = new DigaApiExceptionError(new Exception());
+        assertNull(nullError.asCodeValidationError());
+        assertTrue(error.asInvoiceResponseError() instanceof DigaInvoiceResponseError);
+    }
+
+    @Test
+    void asApiExceptionError() {
+        DigaApiResponseError error = new DigaApiExceptionError(new Exception());
+        DigaApiResponseError nullError = DigaInvoiceResponseError.builder().build();
+        assertNull(nullError.asApiExceptionError());
+        assertTrue(error.asApiExceptionError() instanceof DigaApiExceptionError);
+    }
+}
